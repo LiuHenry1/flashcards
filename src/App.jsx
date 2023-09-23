90e45c38189d81d014bdf9e62221c04864379efd
Henry Liu <hliu4176@gmail.com>
2023-09-23 17:18:03 -0500

    Create lists of Q&As; layout structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import { useState } from 'react'
 import './App.css'
+import Card from './components/Card.jsx'
 
 const App = () => {
+  const questionsAndAnswers = [
+    {q: "What is a set?", a: "An unordered collection of objects"}, 
+    {q: "What are the objects of a set called?", a: "Elements"},
+    {q: "_ is defined as the number of elements in a set", a: "Cardinality"},
+    {q: "If every element in set A is in set B, then A is a _ of B", a: "subset"},
+    {q: "If A is a subset of B, but there exists an element in A not in B, then A is a _ of B", a: "proper subset"},
+    {q: "The _ of A and B is the set of elements in A or B", a: "union" },
+    {q: "The _ of A and B is the set of elements in both A and B", a: "intersection"},
+    {q: "The _ between A is B is the set of elements in A but not B", a: "difference"},
+    {q: "The difference between two sets is not _", a: "commutative"},
+    {q: "However, _ is commutative", a: "symmetric difference"},
+  ]
   return (
     <>
+      <h1>Set Theory Study Guide</h1>
+      <h2>How well do you know your set definitions and laws? Test your knowledge!</h2>
+      <h5>Number of cards: </h5>
     </>
   )
 }
