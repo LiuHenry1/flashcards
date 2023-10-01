502ade14105f4a4c4a3297fd31603c3c92365e99
Henry Liu <hliu4176@gmail.com>
2023-10-01 09:57:06 -0500

    Refactor code

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,7 @@
 const Card = ({ question, answer, isFlipped, handleClick }) => {
+  const className = "card" + (isFlipped ? " flipped" : "");
   return (
-    <div className={"card" + (isFlipped ? " flipped" : "")} onClick={handleClick}>
+    <div className={className} onClick={handleClick}>
       <div className="front">{question}</div>
       <div className="back">{answer}</div>
     </div>
