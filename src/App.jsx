4af7b86742ab17da5d22b69d8e27b498045b5055
Henry Liu <hliu4176@gmail.com>
2023-10-01 12:10:07 -0500

    Pass difficulty as prop to card component

--- src/App.jsx
+++ src/App.jsx
@@ -77,6 +77,7 @@ const App = () => {
       <Card
         question={questionsAndAnswers[current]["q"]}
         answer={questionsAndAnswers[current]["a"]}
+        difficulty={questionsAndAnswers[current]["difficulty"]}
         isFlipped={isFlipped}
         handleClick={handleCardClick}
         key={current}
